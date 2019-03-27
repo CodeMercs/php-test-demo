3ef5059bce9573540f2a3f0b69239db91e66ecef
kancheng <zxdfgcv@gmail.com>
2019-03-27 14:21:10 +0800

    edit

---
+++ test2/se.php
@@ -0,0 +1,67 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<title>Test DB</title>
+	</head>
+	<body>
+	<br />
+
+		<?php
+
+		/* PHP 7 */
+		/* M */
+			$dbhost = 'localhost';
+			$dbuser = 'root';
+			$dbpass = 'hitachi';
+			$dbname = 'test';
+			echo 	"Host : " . $dbhost . "<br /><br />";
+
+			 $con = mysqli_connect( $dbhost, $dbuser, $dbpass, $dbname) or die('Error with MySQL connection');
+
+		?>
+
+
+	<?php
+
+	?>
+	<?php
+	$name = $_POST['name']; 
+
+		$sql = 'SELECT 
+				`user`.`uid` AS `user_id`, 
+				`user`.`passwd` AS `user_passwd`, 
+				`user`.`email` AS `user_email`, 
+				`student`.`name` AS `stu_name`,
+				`student`.`birthday` AS `stu_birth`  
+				FROM `user` 
+				LEFT JOIN `student` ON `user`.`sid`=`student`.`sid` 
+				WHERE `user`.`sid` = '. $name;
+
+		echo  "Show SQL : ". $sql ."<br /><br /><br /> Show Table : <br />";
+
+		$result = $con->query($sql);
+
+		if ($result->num_rows > 0) {
+			// output data of each row
+			while($row = $result->fetch_assoc()) {
+
+				echo "user_id : " . $row["user_id"]. 
+				"; user_passwd : " . $row["user_passwd"] . 
+				"; user_email : " . $row["user_email"] . 
+				"; stu_name : "  . $row["stu_name"] . 
+				"; stu_birth : "  . $row["stu_birth"] . "<br />";
+			}
+		} else {
+			echo "0 results";
+		}
+	echo "<br /><br />Sid : $name";
+	?>
+	<input type="button" value="Back" onclick="location.href='test.php'">
+
+
+	</body>
+</html>
+
+
+
+
---
+++ test2/test.php
@@ -0,0 +1,123 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<title>Test DB</title>
+
+		<?php
+
+		/* PHP 7 */
+		/* M */
+			$dbhost = 'localhost';
+			$dbuser = 'root';
+			$dbpass = 'hitachi';
+			$dbname = 'test';
+			echo 	"Host : " . $dbhost . "<br /><br />";
+
+			// $con = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+			 $con = mysqli_connect( $dbhost, $dbuser, $dbpass, $dbname) or die('Error with MySQL connection');
+			//mysql_connect($dbhost, $dbuser, $dbpass) or die('Error with MySQL connection');
+			
+			//mysql_query("SET NAMES 'utf8'");
+			//mysql_select_db($dbname);
+
+			$sql1 = 'SELECT * from scores;';
+			$sql2 = 'SELECT * from student;';
+			$sql3 = 'SELECT * from user;';
+			$sql4 = 'SELECT `user`.`uid` AS `user_id`, `user`.`passwd` AS `user_passwd`, `user`.`email` AS `user_email`, `student`.`name` AS `stu_name` FROM `user` LEFT JOIN `student` ON `user`.`sid`=`student`.`sid`';
+			//$resu1 = mysqli_query( $con, $sql1) or die('MySQL query error');
+			//$resu2 = mysqli_query( $con, $sql2) or die('MySQL query error');
+
+			//$sc = array();
+			//$st = array();
+		?>
+
+	</head>
+
+	<body>
+
+	<?php 
+
+	/* C & V */
+	if ($con->connect_error) {
+		die("Connection failed : " . $con->connect_error);
+	} 
+
+	// $sql = "SELECT C1, C2, C3 FROM TableName";
+	$result1 = $con->query($sql1);
+
+	echo " Scores <br />";
+	if ($result1->num_rows > 0) {
+		// output data of each row
+		while($row = $result1->fetch_assoc()) {
+		echo "id : " . $row["no"]. "; Name : " . $row["name"]. "; sid : " . $row["sid"]. "<br />";
+		}
+	} else {
+		echo "0 results";
+	}
+
+	echo "<br /><br /><br /> Student <br />";
+	$result2 = $con->query($sql2);
+
+	if ($result2->num_rows > 0) {
+		// output data of each row
+		while($row = $result2->fetch_assoc()) {
+		echo "sid : " . $row["sid"]. "; Name : " . $row["name"]."; Birthday : ". $row["birthday"]. "<br />";
+		}
+	} else {
+		echo "0 results";
+	}
+
+	echo "<br /><br /> User <br />";
+	$result3 = $con->query($sql3);
+
+	if ($result3->num_rows > 0) {
+		// output data of each row
+		while($row = $result3->fetch_assoc()) {
+		echo "uid : " . $row["uid"]. "; passwd : " . $row["passwd"] . "; email : " . $row["email"] . "; sid : "  . $row["sid"] . "<br />";
+		}
+	} else {
+		echo "0 results";
+	}
+
+	echo "<br /><br /> Join <br />";
+	$result4 = $con->query($sql4);
+	if ($result4->num_rows > 0) {
+		// output data of each row
+		while($row = $result4->fetch_assoc()) {
+		echo "user_id : " . $row["user_id"]. "; user_passwd : " . $row["user_passwd"] . "; user_email : " . $row["user_email"] . "; stu_name : "  . $row["stu_name"] . "<br />";
+		}
+	} else {
+		echo "0 results";
+	}
+	echo "<br />";
+
+	?>
+<br />
+	<?php
+
+	$sql5 = 'SELECT `user`.`uid` AS `user_id`, `user`.`passwd` AS `user_passwd`, `user`.`email` AS `user_email`, `student`.`name` AS `stu_name` FROM `user` LEFT JOIN `student` ON `user`.`sid`=`student`.`sid` WHERE `user`.`uid` = 1';
+
+		echo "<br />";
+		$result5 = $con->query($sql5);
+		if ($result5->num_rows > 0) {
+			// output data of each row
+			while($row = $result5->fetch_assoc()) {
+			echo "user_id : " . $row["user_id"]. "; user_passwd : " . $row["user_passwd"] . "; user_email : " . $row["user_email"] . "; stu_name : "  . $row["stu_name"] . "<br />";
+			}
+		} else {
+			echo "0 results";
+		}
+	?>
+
+		<form action="se.php" method="post">
+			Sid Search :
+			<input type ="text" name="name" value="2">
+			<input type ="submit" value="送出">
+		</form>
+	</body>
+</html>
+
+
+
+
+
