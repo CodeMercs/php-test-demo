3967372f990041959cd555ddf7fad6f1efe56ebc
kancheng <zxdfgcv@gmail.com>
2019-07-04 17:10:53 +0800

    edit

---
+++ test-stripslashes/index.php
@@ -0,0 +1,14 @@
+<?php 
+$str = "Is your name O\'reilly?";
+
+// Outputs: Is your name O'reilly?
+
+
+echo "Raw : " . $str . "<br \><br \>";
+echo "Stripslashes - stripslashes(): " . stripslashes($str) . " <br \><br \>";
+
+
+
+ ?>
+
+
